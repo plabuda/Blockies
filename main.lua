98cb4b389ce6177129ad62fec108bfe4d4ec0e5a
Piotr Łabuda <piotr.jakub.labuda@gmail.com>
2020-10-27 00:19:49 +0100

    margins rework

--- main.lua
+++ main.lua
@@ -18,15 +18,21 @@ box.new = function( measure_callback, draw_callback )
         child = nil
     }
 
-    retval.measure = function () 
-        measure_callback ( retval )
+    retval.getSize = function ()        
         return retval.w + retval.m_w, retval.h + retval.m_h
+    end
+
+    retval.measure = function () 
+        if ( retval.child ) then
+            retval.child.measure()
+        end
+        measure_callback ( retval )
     end
 
     retval.draw = function ( x, y )
         draw_callback ( retval, x, y )
         if ( retval.child ) then
-            retval.child.draw( retval.x + x, retval.y + y)
+            retval.child.draw( retval.x + retval.m_w /2 + x , retval.y + retval.m_h /2 + y)
         end
     end
 
@@ -35,15 +41,16 @@ end
 
 local function red_block()
     local measure_callback = function ( retval )
-        retval.w = 200
-        retval.h = 50       
-        retval.x = 50
-        retval.y = 200
+        local w, h = retval.child.getSize()
+        retval.x = 200
+        retval.h = retval.child.y + h       
+        retval.y = 50
+        retval.w = retval.child.x + w
     end
 
     local draw_callback = function ( retval, x, y )
         love.graphics.setColor(0.5,0,0)
-        love.graphics.rectangle('line', x + retval.x, y + retval.y, retval.w + retval.m_w, retval.h + retval.m_h)
+        love.graphics.rectangle('line', x + retval.x , y + retval.y , retval.w + retval.m_w, retval.h + retval.m_h)
         love.graphics.setColor(0.7,0,0)
         love.graphics.rectangle('fill', x + retval.x + retval.m_w / 2, y + retval.y + retval.m_h / 2, retval.w, retval.h)
     end
@@ -55,8 +62,10 @@ local function yellow_block()
     local measure_callback = function ( retval )
         retval.w = 50
         retval.h = 50       
-        retval.x = 50
-        retval.y = 50
+        retval.x = 150
+        retval.y = 250
+        retval.m_w = retval.m_w * -1
+        retval.m_h = retval.m_w
     end
 
     local draw_callback = function ( retval, x, y )
@@ -80,8 +89,4 @@ end
 
 function love.keypressed ()
     red.measure()
-end
-
-function love.keyreleased ()
-    yell.measure()
 end
