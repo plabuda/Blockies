cb9043c734cf5c5b444235a87f507b98b32162ac
Piotr Łabuda <piotr.jakub.labuda@gmail.com>
2020-10-27 01:52:21 +0100

    Fix for payloads

--- main.lua
+++ main.lua
@@ -43,7 +43,7 @@ box.new = function( measure_callback, draw_callback )
         -- iterate over and measure all collections
         for j=1,#retval.collections do
             if retval.collections[j].payload then
-                local collection = retval.collections[j]
+                local collection = retval.collections[j].payload
                 for k=1,#collection do
                     collection[k].measure()
                 end
@@ -68,7 +68,7 @@ box.new = function( measure_callback, draw_callback )
         -- iterate over and measure all collections
         for j=1,#retval.collections do
             if retval.collections[j].payload then
-                local collection = retval.collections[j]
+                local collection = retval.collections[j].payload
                 for k=1,#collection do
                     collection[k].draw(x2, y2)
                 end
@@ -80,56 +80,69 @@ box.new = function( measure_callback, draw_callback )
     return retval
 end
 
-local function red_block()
+local function random_block()
+    local r = love.math.random( )
+    local g = love.math.random( )
+    local b = love.math.random( )
+    local w = 20 + love.math.random( ) * 180
+    local h = 20 + love.math.random( ) * 10
+
     local measure_callback = function ( retval )
-        local child = retval.children[1].payload
-        local w, h = child.getSize()
-        retval.x = 200
-        retval.h = child.y + h       
-        retval.y = 50
-        retval.w = child.x + w
+        retval.h = h
+        retval.w = w
     end
 
     local draw_callback = function ( retval, x, y )
-        love.graphics.setColor(0.5,0,0)
+        love.graphics.setColor(r * 0.5, g * 0.5, b * 0.5)
         love.graphics.rectangle('line', x + retval.x , y + retval.y , retval.w + retval.m_w, retval.h + retval.m_h)
-        love.graphics.setColor(0.7,0,0)
+        love.graphics.setColor(r * 0.7, g * 0.7, b * 0.7)
         love.graphics.rectangle('fill', x + retval.x + retval.m_w / 2, y + retval.y + retval.m_h / 2, retval.w, retval.h)
     end
 
     return box.new( measure_callback, draw_callback )
 end
 
-local function yellow_block()
+local function horizontal_block()
     local measure_callback = function ( retval )
-        retval.w = 50
-        retval.h = 50       
-        retval.x = 150
-        retval.y = 250
-        retval.m_w = retval.m_w * -1
-        retval.m_h = retval.m_w
+        local offset = 0
+        local max_h = 50
+        for i=1,#retval.blocks.payload do 
+            local block = retval.blocks.payload[i]
+            block.y = MARGIN_DEFAULT / 2
+            block.x = offset -- + MARGIN_DEFAULT /2
+            local w, h = block.getSize()
+            max_h = math.max( max_h, h )
+            offset = offset + w
+        end --named component
+            
+        retval.h = max_h + MARGIN_DEFAULT  
+        retval.w = offset -- + MARGIN_DEFAULT
     end
 
     local draw_callback = function ( retval, x, y )
-        love.graphics.setColor(0.5,0.5,0)
-        love.graphics.rectangle('line', x + retval.x, y + retval.y, retval.w + retval.m_w, retval.h + retval.m_h)
-        love.graphics.setColor(0.7,0.7,0)
+        love.graphics.setColor(0.2,0.2,0.2)
+        love.graphics.rectangle('line', x + retval.x , y + retval.y , retval.w + retval.m_w, retval.h + retval.m_h)
+        love.graphics.setColor(0.35,0.35,0.35)
         love.graphics.rectangle('fill', x + retval.x + retval.m_w / 2, y + retval.y + retval.m_h / 2, retval.w, retval.h)
     end
 
-    return box.new( measure_callback, draw_callback )
+    local result = box.new( measure_callback, draw_callback)
+    local collection = new_collection(nil)
+    result.blocks = collection
+    result.collections = {collection}
+
+    return result
+
 end
 
-local red = red_block()
-local yell = yellow_block()
-
-red.children = { new_child(yell) }
+local hor = horizontal_block()
+hor.blocks.payload = {random_block(), random_block(), random_block(), random_block(), random_block() }
 
 
 function  love.draw ( ... )
-    red.draw(50, 50)
+    hor.draw(50,50)
 end
 
 function love.keypressed ()
-    red.measure()
+    hor.measure()
 end
